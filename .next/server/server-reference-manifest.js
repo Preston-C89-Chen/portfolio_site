19da4db4a84ee88d7aedad5e3077d447eba6f701
Preston Chen <prestonchen@Prestons-MacBook-Pro.local>
2024-09-16 17:10:47 -0700

    remove next

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"iLYqIjeubcX9M2xVtKZpsxa8hR/i+ekJ3Jc02+mVkqE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"szIBmCZrv3yGzjb6ascskFt2JblOCneAX+gW0xgY0mM=\"\n}"
